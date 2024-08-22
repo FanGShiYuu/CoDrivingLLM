e50dd8a9038e1c1dc8c3c8075422daf85499cefa
Shiyu Fang <88468647+FanGShiYuu@users.noreply.github.com>
2024-08-22 17:24:41 +0800

    Delete highway_env directory

--- highway_env/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from highway_env.envs.merge_env_v1 import *
-# from highway_env.envs.highway_env import *
-from highway_env.envs.intersection_env import *
-from highway_env.envs.highway_env import *
